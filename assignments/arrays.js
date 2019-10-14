9d2643452f1686de9658b0beed22521f2b0ba97d
Rae Glazier <glazier.home.life@gmail.com>
2019-10-14 16:56:26 -0700

    completed array challenge 3

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -97,9 +97,29 @@ console.log(lastCar.car_make, lastCar.car_model);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
+
+// inventory.sort();
+// console.log(inventory);
+
 let carModels = [];
 let carModelsSorted = [];
-console.log();
+// console.log();
+
+for (let i = 0; i < inventory.length; i++) {
+
+ carModels.push(inventory[i].car_model); 
+
+  //{carModels.push(inventory[i])};
+
+};
+
+console.log(carModels.sort());
+
+
+
+// let carModels = [];
+// let carModelsSorted = [];
+// console.log();
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
